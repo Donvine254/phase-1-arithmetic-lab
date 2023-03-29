2973a9c087c379704f912024a61aa136f8f025fa
donvine mugendi <donvinemugendi@gmail.com>
2023-03-29 23:47:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1=2;
+const num2=31;
+const multiply=num1*num2;
+const random = Math.round(Math.random() * 100) + 1;
+const num3 = 14;
+const num4 = 5;
+const mod = Math.floor(num3 % num4) === 4 ? 4 : null;
+const max=Math.max(3,4,5,20)
